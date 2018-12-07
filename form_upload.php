c83efc1a52bf6610ce5ab9f11f7dd6dbac4b03aa
mnurulamri <mnurulamri@gmail.com>
2018-12-07 18:06:45 +0700

    update form upload

--- form_upload.php
+++ form_upload.php
@@ -11,7 +11,70 @@ if(!empty($_FILES)) {
   $allowed = array('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
 							'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
 							'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 
-							'application/msword',
+						<?php
+include('../models/conn.php');
+$array_type = array('application/msword'=>'doc', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'=>'docx');
+
+$file_kode = $_POST['post_file_kode'];
+
+if(!empty($_FILES)) {
+  $dir_base = "../dokumen/";
+  //filter extentions
+  $fileExt = explode('.', $_FILES['file_upload']['name']); //ada perbedaan antara type dan ext file
+  //$file_ext = $fileExt[1]; //gak jalan
+  $fileActualExt = strtolower(end($fileExt));
+  $file_type = $_FILES['file_upload']['type'];
+  $file_ext = $array_type[$file_type];
+  $allowed = array('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+              'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+              'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 
+              'application/msword',
+              'application/proses',
+              'application/download',
+              'application/pdf',
+              'doc', 'docx');
+  if (in_array($file_type, $allowed)) {   //bisa juga pake -> in_array($fileActualExt, $allowed)
+    if(is_uploaded_file($_FILES['file_upload']['tmp_name'])) {
+      
+      $lokasi_file = '../dokumen/'.$file_kode.'.'.$file_ext;
+    
+      //proses jika ukuran file kurang dari 1 mega
+      if($_FILES['file_upload']['size'] < 3019764){
+        //set variabel
+        $file_size = $_FILES['file_upload']['size'];
+        $file_type = $_FILES['file_upload']['type'];
+        $tgl_upload = date('Y-m-d');
+        $fileExt = explode('.', $_FILES['file_upload']['name']); //ada perbedaan antara type dan ext file
+      
+      //upload file dan update database
+      if(move_uploaded_file($_FILES['file_upload']['tmp_name'], $lokasi_file)) {
+        // return path
+            //echo '<img src="'.$dir_base.$_FILES['file_upload']['name'].'" width="100%"  />';
+        $sql = "UPDATE file_ijk 
+              SET tgl_upload = '$tgl_upload', file_ext = '$file_ext', file_size = '$file_size',  file_type = '$file_type'
+              WHERE file_kode = '$file_kode'";
+        
+        $in = mysql_query($sql) or die('error: '.mysql_error());        
+        if($in){
+          echo '<div class="ok">SUCCESS: File berhasil di Upload!</div>';     
+        }
+        echo '<iframe src="https://docs.google.com/gview?url=https://remun.ppaa.fisip.ui.ac.id/ijk/dokumen/'.$file_kode.'.'.$file_ext.'&embedded=true" frameborder="0" style="width="500" height="500"></iframe>
+        <style>.embed-responsive-10by1 {padding-top: 100%;}</style>';
+      } else {
+          echo '<div class="error">ERROR: Gagal upload file! - '.$lokasi_file.'</div>';
+      } //end of move_uploaded_file
+      
+      } //end of $_FILES
+
+    } // end of is_uploaded_file
+    
+  } else {
+    echo '<span style="color:red">You cannot upload files of this type!</span>';
+  } //end of in_array
+  
+} //end of empty $_FILES
+
+?>	'application/msword',
 							'application/proses',
 							'application/download',
 							'application/pdf',
