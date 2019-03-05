88459b924f1f9956db2f6d39230878380d6c29e8
mnurulamri <mnurulamri@gmail.com>
2019-03-05 19:22:18 +0700

    05 Maret 2019

--- form_lembur_data_pemohon.php
+++ form_lembur_data_pemohon.php
@@ -2,9 +2,6 @@
 #include("../models/conn2.php"); -> untuk knyetak msword pake ini nggak jalan ????
 #include("https://remun.ppaa.fisip.ui/ijk/libraries/unit_Kerja.php");
 
-$mysqli = mysql_connect($server,$user,$pass) or die('error connecting mysql');
-mysql_select_db($db,$mysqli) or die('Database tidak ditemukan');
-
 if(!session_id()) session_start();
 $nip = $_SESSION['user_nip'];
 
